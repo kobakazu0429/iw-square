d8cb90f5eac39999d949182172df9b1449fc2390
kobakakzu0429 <kobakazu10@gmail.com>
2020-06-25 15:08:21 +0900

    add creators admin page

---
+++ src/components/admin/CreatorsTable.tsx
@@ -0,0 +1,66 @@
+import React, { FC } from "react";
+import {
+  Table,
+  TableHeader,
+  TableHeaderCell,
+  TableBody,
+  TableRow,
+  TableCell,
+  Button,
+  Image,
+} from "semantic-ui-react";
+import { Image as CloudinaryImage, Transformation } from "cloudinary-react";
+
+import { Creator } from "../../microcms/type";
+
+// TODO: eslint@7.3でcreatorのエラーが治るはず
+const CreatorTableRow: FC<{ creator: Creator }> = (props) => {
+  return (
+    <TableRow>
+      <TableCell>{props.creator.id}</TableCell>
+      <TableCell>
+        <Image avatar>
+          <CloudinaryImage cloudName="iw-square" publicId={props.creator.icon}>
+            <Transformation crop="fill" height="100" width="100" />
+          </CloudinaryImage>
+        </Image>
+        <span>{props.creator.name}</span>
+      </TableCell>
+      <TableCell>
+        <a href={`https://twitter.com/${props.creator.twitter_id}`}>
+          {props.creator.twitter_id}
+        </a>
+      </TableCell>
+      <TableCell>
+        <a href={`https://www.facebook.com/${props.creator.facebook_id}`}>
+          {props.creator.facebook_id}
+        </a>
+      </TableCell>
+      <TableCell>
+        <Button icon="edit" content="編集する" labelPosition="left" />
+      </TableCell>
+    </TableRow>
+  );
+};
+
+export const CreatorsTable: FC<{ creators: Creator[] }> = (props) => {
+  return (
+    <Table striped>
+      <TableHeader>
+        <TableRow>
+          <TableHeaderCell>コンテンツID</TableHeaderCell>
+          <TableHeaderCell>名前</TableHeaderCell>
+          <TableHeaderCell>Twitter</TableHeaderCell>
+          <TableHeaderCell>Facebook</TableHeaderCell>
+          <TableHeaderCell></TableHeaderCell>
+        </TableRow>
+      </TableHeader>
+
+      <TableBody>
+        {props.creators.map((creator) => (
+          <CreatorTableRow key={creator.id} creator={creator} />
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
--- src/pages/admin/creators.tsx
+++ src/pages/admin/creators.tsx
@@ -1,11 +1,36 @@
 import React from "react";
 import { AdminContainer } from "../../components/admin/Container";
+import { GetServerSidePropsContext, GetServerSidePropsResult } from "next";
+import { Creator } from "../../microcms/type";
+import { CreatorsTable } from "../../components/admin/CreatorsTable";
+import { creatorsClient } from "../../microcms/creators";
+
+interface ServerSideProps {
+  creators: Creator[];
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface Props extends ServerSideProps {}
 
 // eslint-disable-next-line react/display-name
-export default () => {
+export default (props: Props) => {
   return (
     <AdminContainer>
-      <h1>creator</h1>
+      <div>
+        <h1>works</h1>
+        <CreatorsTable creators={props.creators} />
+      </div>
     </AdminContainer>
   );
 };
+
+export async function getServerSideProps(
+  _ctx: GetServerSidePropsContext
+): Promise<GetServerSidePropsResult<ServerSideProps>> {
+  const res = await creatorsClient.fetchAllCreators();
+  return {
+    props: {
+      creators: res,
+    },
+  };
+}
