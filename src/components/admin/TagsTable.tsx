aba46554c88a00751ec69a878524f2368dcc5f31
kobakakzu0429 <kobakazu10@gmail.com>
2020-06-26 02:29:25 +0900

    impl tag edit modal

--- src/components/admin/TagsTable.tsx
+++ src/components/admin/TagsTable.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState, useCallback } from "react";
 import {
   Table,
   TableHeader,
@@ -7,9 +7,107 @@ import {
   TableRow,
   TableCell,
   Button,
+  Modal,
+  Form,
+  Input,
+  Grid,
+  Icon,
 } from "semantic-ui-react";
+
 import { Tag } from "../../microcms/type";
 import { toJST } from "../../utils/toJST";
+
+const EditModal: FC<{ trigger: React.ReactNode; tag: Tag }> = (props) => {
+  const [open, setOpen] = useState(false);
+
+  const onOpen = useCallback(() => {
+    setOpen(true);
+  }, []);
+
+  const onClose = useCallback(() => {
+    setOpen(false);
+  }, []);
+
+  return (
+    <Modal
+      trigger={
+        <Button
+          icon="edit"
+          content="編集する"
+          labelPosition="left"
+          onClick={onOpen}
+        />
+      }
+      size="tiny"
+      open={open}
+    >
+      <Modal.Header>タグを編集 (コンテンツID: {props.tag.id})</Modal.Header>
+      <Modal.Content>
+        <Modal.Description>
+          <Form>
+            <Grid columns="two">
+              <Grid.Row>
+                <Grid.Column>
+                  <label>コンテンツID</label>
+                </Grid.Column>
+
+                <Grid.Column>
+                  <Form.Field>
+                    <Input readOnly placeholder={props.tag.id} />
+                  </Form.Field>
+                </Grid.Column>
+              </Grid.Row>
+
+              <Grid.Row>
+                <Grid.Column>
+                  <label>タグ</label>
+                </Grid.Column>
+
+                <Grid.Column>
+                  <Form.Field>
+                    <Input focus placeholder={props.tag.tag} />
+                  </Form.Field>
+                </Grid.Column>
+              </Grid.Row>
+
+              <Grid.Row>
+                <Grid.Column>
+                  <label>作成日</label>
+                </Grid.Column>
+
+                <Grid.Column>
+                  <Form.Field>
+                    <Input readOnly placeholder={props.tag.updatedAt} />
+                  </Form.Field>
+                </Grid.Column>
+              </Grid.Row>
+
+              <Grid.Row>
+                <Grid.Column>
+                  <label>更新日</label>
+                </Grid.Column>
+
+                <Grid.Column>
+                  <Form.Field>
+                    <Input readOnly placeholder={props.tag.updatedAt} />
+                  </Form.Field>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Form>
+        </Modal.Description>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button basic color="red" onClick={onClose}>
+          <Icon name="remove" /> キャンセル
+        </Button>
+        <Button color="green" onClick={onClose}>
+          <Icon name="checkmark" /> 保存
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  );
+};
 
 // TODO: eslint@7.3でtagのエラーが治るはず
 const TagTableRow: FC<{ tag: Tag }> = (props) => {
@@ -20,7 +118,7 @@ const TagTableRow: FC<{ tag: Tag }> = (props) => {
       <TableCell>{toJST(props.tag.createdAt)}</TableCell>
       <TableCell>{toJST(props.tag.updatedAt)}</TableCell>
       <TableCell>
-        <Button icon="edit" content="編集する" labelPosition="left" />
+        <EditModal tag={props.tag}></EditModal>
       </TableCell>
     </TableRow>
   );
